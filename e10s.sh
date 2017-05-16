d279ae859db7b7652466cc38689cb1ace38de031
benmiroglio <bmiroglio@mozilla.com>
2017-05-16 12:02:50 -0700

    Add report generator

--- e10s.sh
+++ e10s.sh
@@ -2,21 +2,19 @@ week="$1"
 start="$2"
 end="$3"
 
-# echo "CLONING REPOS"
-# # clone repos to /mnt
-# cd /mnt && git clone https://github.com/mozilla/telemetry-batch-view.git && git clone https://github.com/benmiroglio/temp-aws-tools.git
+echo "CLONING REPOS"
+# clone repos to /mnt
+cd /mnt && git clone https://github.com/mozilla/telemetry-batch-view.git && git clone https://github.com/benmiroglio/temp-aws-tools.git
 
-# # update e10s script
-# # (handles edge case for JNothing Error and grabs the submission field)
-# cat temp-aws-tools/E10sExperiment.scala > telemetry-batch-view/src/main/scala/com/mozilla/telemetry/views/E10sExperiment.scala
+# update e10s script
+# (handles edge case for JNothing Error and grabs the submission field)
+cat temp-aws-tools/E10sExperiment.scala > telemetry-batch-view/src/main/scala/com/mozilla/telemetry/views/E10sExperiment.scala
 
-# echo BUILDING SCALA CODE
+echo BUILDING SCALA CODE
 
-# # build code
-# cd telemetry-batch-view && sbt assembly
+# build code
+cd telemetry-batch-view && sbt assembly
 
-# # open screen process (crl A+D to return)
-# screen 
 
 
 # echo RUNNING ETL
@@ -39,25 +37,23 @@ cd /home/hadoop/analyses/e10s_analyses/multi/beta/54
 mkdir $week
 
 # get the previous week name
-prev=week$(($week - 1))
-echo Copying notebook from $prev...
+prev=$((${week//[!0-9]/} - 1))
+echo Copying notebook from week$prev...
 
 
-cp $prev/e10sMulti_experiment.ipynb $week/
+cp week$prev/e10sMulti_experiment.ipynb $week/
 cd $week
+pwd
 
 # insert new date range into notebook
 python /mnt/temp-aws-tools/insert_range.py $start $end
 
 echo RUNNING NOTEBOOK
 # run noteook and render to html
-jupyter nbconvert \
-    --ExecutePreprocessor.kernel_name=python2\
-    --ExecutePreprocessor.timeout=-1\
-    --log-level=10\
-    --execute e10sMulti_experiment.ipynb\
-    --to html\
-    --output-dir ./html/
+time env $environment \
+PYSPARK_DRIVER_PYTHON=jupyter
+PYSPARK_DRIVER_PYTHON_OPTS="nbconvert --ExecutePreprocessor.kernel_name=python2 --ExecutePreprocessor.timeout=-1 --log-level=10 --execute e10sMulti_experiment.ipynb --to html --output-dir ./html/" \
+pyspark
 
 # copy html files to s3
 aws s3 cp --recursive ./html s3://telemetry-test-bucket/e10s_experiment_view/multi_webExtensions_beta54_cohorts/$week-html
---
+++ generate_report.py
@@ -0,0 +1,75 @@
+import sys
+import re
+
+target_week = int(re.sub('[a-z]', '', sys.argv[1]))
+weeks = ['week' + str(i) for i in range(1, target_week+1)][::-1]
+print 
+
+
+def get_text(filename):
+	with open(filename) as f:
+		return f.read()
+
+def write_to_file(contents, filename):
+	with open(filename, 'w') as out:
+		out.write(contents)
+
+def probe_header():
+	return '''
+		<link rel="stylesheet" type="text/css" href="../notebook.css">
+		<body>
+	
+		<div class="cell border-box-sizing text_cell rendered">
+		<div class="prompt input_prompt">
+		</div>
+		<div class="inner_cell">
+		<div class="text_cell_render border-box-sizing rendered_html">
+	'''
+
+
+def any_keywords(line):
+	lst = ["Wrangling", "<hr>", "MemoryUsage", "TabSwitching", "SlowScripts"]
+	return any([i in line for i in lst])
+
+
+def reformat_report(filename, directory):
+	path = '/'.join([directory, filename])
+	with open(path) as f:
+		current_html = ''
+		current_probe = None
+		first = True
+		for line in f:
+			if first and '<h1 id="Results">' in line:
+				first = False
+				write_to_file(current_html, '/'.join([directory, 'summary.html']))
+			if first:
+				current_html += line
+				continue
+			if line[:3] == '<h3':
+				print current_probe
+				if current_probe:
+				  write_to_file(current_html, '/'.join([directory, current_probe + '.html']))
+				current_probe = line.split('=')[1].split('>')[0].strip('"')
+				current_html = probe_header()
+			elif any_keywords(line) and current_probe:
+				write_to_file(current_html, '/'.join([directory, current_probe + '.html']))
+
+			else:
+				current_html +=  line 
+				
+
+
+
+with open('multi-new.Rmd', 'w') as new_report:
+	header =  get_text("./header-markdown-template.md")
+	new_report.write(header)
+	for week in weeks:
+		reformat_report('e10sMulti_experiment.html', 'data/{}'.format(week))
+		week_body = re.sub("{}", week, get_text("weekx-markdown-template.md"))
+		new_report.write(week_body)
+
+
+print "Done"
+
+
+	
