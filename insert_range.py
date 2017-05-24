b87d4e4861062ab45b4c56cb7d8fd6ff143df458
benmiroglio <bmiroglio@mozilla.com>
2017-05-24 16:21:39 +0000

    Make weekly file generation independent

--- insert_range.py
+++ insert_range.py
@@ -1,16 +1,12 @@
 import json
 import sys
-import datetime
+import re
+import datetime as dt
 
-def update_range(directory):
-    with open(directory + "/last.json") as f:
-        date_range = json.load(f)
-    start, end = date_range['range'][1:].split('_')
-    start = end
-    end = datetime.datetime.strftime(
-                datetime.datetime.strptime(end, '%Y%m%d') + \
-                datetime.timedelta(days=7), '%Y%m%d')
-    return start,end
+def check_params():
+    assert len(sys.argv) == 3, "must specific start and end dates"
+    assert len(sys.argv[1]) == len(sys.argv[2]) == 8, "invalid start or end date"
+    return sys.argv[1], sys.argv[2]
 
 def edit_notebook(start, end):
     with open('e10sMulti_experiment.ipynb') as f:
@@ -21,10 +17,27 @@ def edit_notebook(start, end):
         src = cell['source']
         if len(src) > 0:
             if "RANGE" in src[0]:
-                notebook['cells'][i]['source'][0] = "RANGE = " + '"v' + '_'.join([start, end]) + '"\n'
+                insert = "RANGE = " + '"v' + '_'.join([start, end]) + '"\n'
+                print "inserting the following line: {}".format(insert)
+                notebook['cells'][i]['source'][0] = insert
     with open('e10sMulti_experiment.ipynb', 'w') as output:
         json.dump(notebook, output)
 
-if __name__ == "__main__":
-    print 'done'
 
+def update_params():
+    path = '/home/hadoop/analyses/e10s_analyses/multi/meta/last.json'
+    with open(path) as f:
+        jobj = json.load(f)
+        jobj['week'] = 'week' + str(int(re.sub('[a-z]', '',  jobj['week'])) + 1)
+        jobj['start'] = jobj['end']
+        jobj['end'] = dt.datetime.strftime(
+                                dt.datetime.strptime(jobj['end'], '%Y%m%d') + dt.timedelta(days=7), "%Y%m%d")
+    with open(path, 'w') as out:
+        json.dump(jobj, out)
+
+
+if __name__ == "__main__":
+    START, END = check_params()
+    #update_params()
+    edit_notebook(START, END)
+    
