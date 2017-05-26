53102d33b55b21c27d08d7753dc40dbed64bde65
benmiroglio <bmiroglio@mozilla.com>
2017-05-26 09:15:34 -0700

    run script in repo

--- e10s.sh
+++ e10s.sh
@@ -1,93 +1,75 @@
+#!/bin/bash
+## The cron job to update the daily reports for the Unified Search v2 study.
+## This is meant to be run as a cron from hala1.
+
 set -e
 
-echo "V3 of e10s.sh"
+. ~/.bash_profile
 
-echo "Updating E10s Repos..."
-cd $HOME/analyses/e10s_analyses/
-git pull origin master
+TODAY=$(date +%Y-%m-%d)
+CLUSTER_JOB_DIR=$HOME
+## Time between checks for job completion.
+COMPLETION_CHECK_INTERVAL_MINUTES=10
+## This file in the current dir contains the cluster ID.
+## This filename is shared with the R scripts (for now).
+CLUSTER_ID_FILE=e10s-emr
 
-cd $HOME/analyses/temp-aws-tools/
-git pull origin master
+## Patch to load "methods" package when using Rscript.
+## It is not loaded by default.
+RSCRIPT='Rscript --default-packages=methods,datasets,utils,grDevices,graphics,stats'
 
-MULTI_DIR="$HOME/analyses/e10s_analyses/multi"
+## On error, send email notification and exit.
+fail() {
+    ## Send email notification if there email address is available.
+    [[ -n "$SCRIPT_ERROR_EMAIL" && "$SCRIPT_ERROR_EMAIL" =~ "@" ]] && \
+        echo "$1" | mailx -s "FAILED: Unified Search daily job" \
+            "$SCRIPT_ERROR_EMAIL"
+    echo -e "\nScript failed: $1"
+    echo "Exiting: `date`."
+    exit 1
+}
 
-week=$(cat $HOME/analyses/e10s_analyses/multi/meta/last.json | python -c "import sys, json; print json.load(sys.stdin)['week']")
-start=$(cat $HOME/analyses/e10s_analyses/multi/meta/last.json | python -c "import sys, json; print json.load(sys.stdin)['start']")
-end=$(cat $HOME/analyses/e10s_analyses/multi/meta/last.json | python -c "import sys, json; print json.load(sys.stdin)['end']")
+## Outputs for each run are stored in a dir named using the current date.
 
-echo "===================================================================="
-echo "Running job for $week over the following range: $start-$end"
-echo "===================================================================="
+## Redirect output if required.
+exec > cronjob.log 2>&1
 
-echo "\n\nCloning telemetry-batch-view...\n\n"
-rm -rf /mnt/telemetry-batch-view/
-cd /mnt/
-git clone https://github.com/benmiroglio/telemetry-batch-view.git
+## Use mozaws to start a 1-node Spark cluster.
+echo -e "\nLaunching Spark cluster: `date`."
+R_START_CLUSTER="source('~/mozaws.init.R'); cl <- emr.new(spotnodes=19);"
+R_START_CLUSTER="$R_START_CLUSTER cat(cl\$Id, file = '$CLUSTER_ID_FILE');"
+$RSCRIPT -e "$R_START_CLUSTER"
+[[ $? -ne 0 ]] && fail "Error starting Spark cluster."
 
-# update e10s script
-# (handles edge case for JNothing Error and grabs the submission field)
-cat $HOME/analyses/temp-aws-tools/E10sExperiment.scala > /mnt/telemetry-batch-view/src/main/scala/com/mozilla/telemetry/views/E10sExperiment.scala
+## Set the job running on the cluster in a detached tmux session.
+CLUSTER_IP_PATH="$HOME/.mozaws.clus/$(cat $CLUSTER_ID_FILE)/dns_name"
+CLUSTER_IP=$(cat "$CLUSTER_IP_PATH")
+DONE_FILE="$CLUSTER_JOB_DIR/_done"
+## Make sure any previous '_done' file is removed.
+ssh hadoop@$CLUSTER_IP rm -f $DONE_FILE
 
-echo "Building scala code..."
+echo "SSH'ing into cluster"
+REMOTE_CMD=". /etc/profile; bash $HOME/analyses/temp-aws-tools/e10s.sh > notebookrun.log 2>&1"
+ssh "hadoop@$CLUSTER_IP" "tmux new -d '$REMOTE_CMD'"
 
-# # build code
-cd /mnt/telemetry-batch-view && sbt assembly
-
-echo "Running scala job..."
-# submit ETL job
-spark-submit\
-    --master yarn\
-    --deploy-mode client\
-    --class com.mozilla.telemetry.views.E10sExperimentView\
-    target/scala-2.11/telemetry-batch-view-1.1.jar\
-    --from $start\
-    --to $end\
-    --channel beta\
-    --version 54.0\
-    --experiment multi-webExtensions-beta54-cohorts\
-    --bucket telemetry-parquet
-
-echo Configuring analyses FOR $week
-cd $HOME/analyses/e10s_analyses/multi/beta/54/
-
-rm -rf $week
-mkdir $week
-
-
-# copy template notebook to current week dir
-cp $HOME/analyses/e10s_analyses/multi/meta/e10sMulti_experiment.ipynb $week/
-cd $week
-
-echo "\n\nRunning notebook in the following directory:\n\n"
-pwd
-
-# insert new date range into notebook
-echo "Updating date range in notebook..."
-python $HOME/analyses/temp-aws-tools/insert_range.py $start $end
-
-echo "Running notebook and converting to HTML..."
-time env $environment \
-PYSPARK_DRIVER_PYTHON=jupyter
-PYSPARK_DRIVER_PYTHON_OPTS="nbconvert --ExecutePreprocessor.kernel_name=python2 --ExecutePreprocessor.timeout=-1 --log-level=10 --execute e10sMulti_experiment.ipynb --to html --output-dir ./html/" \
-pyspark
+## Periodically check for completion, indicated by the presence of the file '_done'.
+NUM_TRIES=0
+while ( ! ssh hadoop@$CLUSTER_IP test -e $DONE_FILE && [[ $NUM_TRIES -lt 40 ]] ); do
+    echo "Checking $NUM_TRIES..." 
+    NUM_TRIES=$((NUM_TRIES+1))
+    sleep "${COMPLETION_CHECK_INTERVAL_MINUTES}m"
+done
+## Remove the '_done' file.
+ssh $CLUSTER_IP rm $DONE_FILE
 
 
-
-echo "Generating RMD File and pushing to S3...RMD will be rendered on dashboard1..."
-
-pwd
-python $HOME/analyses/temp-aws-tools/generate_report.py $week
-
-cp -r $MULTI_DIR/beta/54/$week $HOME/e10s_report/
-
-mv temp.Rmd e10sMulti_experiment.Rmd
-cp e10sMulti_experiment.Rmd $HOME/e10s_report/
-
-aws s3 cp --recursive $HOME/e10s_report/ s3://telemetry-test-bucket/bmiroglio/multi-report/
+## Use mozaws to kill the cluster.
+echo "Killing Spark cluster: `date`."
+R_KILL_CLUSTER="source('~/mozaws.init.R'); clusId <- readLines('$CLUSTER_ID_FILE', n = 1);"
+R_KILL_CLUSTER="$R_KILL_CLUSTER cl <- as.awsCluster(clusId); emr.kill(cl);"
+$RSCRIPT -e "$R_KILL_CLUSTER"
+[[ $? -ne 0 ]] && fail "Error killing Spark cluster."
 
 
-
-
-
-
+echo "Job was Successful :)"
 
